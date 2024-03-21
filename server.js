9d19258276622ce4905e7b2c93f3c2b3cbf91723
Gokuaf <ashsh1583@gmail.com>
2024-03-21 23:58:06 +0530

    wroking on mongodb

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+    //  .connect(process.env.DATABASE_LOCAL, { // FOR LOCAL DATABASE //
+  .connect(DB, {      // FOR ATLAS //
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successfull'));
 
 // console.log(process.env);
 
